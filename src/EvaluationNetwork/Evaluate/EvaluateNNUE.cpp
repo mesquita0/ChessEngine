b70633168bef980a8a65aaa69a149f236f0ee735
Kaynã Mesquita <mesquitakayna@gmail.com>
2024-11-08 12:26:20 -0300

    Implement NNUE evaluation

--- src/EvaluationNetwork/Evaluate/EvaluateNNUE.cpp
+++ src/EvaluationNetwork/Evaluate/EvaluateNNUE.cpp
@@ -5,37 +5,56 @@
 #include "LinearLayer.h"
 #include <array>
 #include <cstdint>
+#include <filesystem>
 
 constexpr double scale = 128;
 
 NNUE::NNUE() {
+	std::filesystem::path weights_dir = std::filesystem::path(__FILE__).parent_path().parent_path() / "Weights";
 
+	accumulator.setWeights(weights_dir / "accb.bin", weights_dir / "accw.bin");
+	hidden_layer1.setWeights(weights_dir / "lin1b.bin", weights_dir / "lin1w.bin");
+	hidden_layer2.setWeights(weights_dir / "lin2b.bin", weights_dir / "lin2w.bin");
+	hidden_layer3.setWeights(weights_dir / "lin3b.bin", weights_dir / "lin3w.bin");
+
+	// TODO: Throw error if files not found
 }
 
 int NNUE::evaluate() {
 
+	// Quantitize accumulator
+	crelu(accumulator.arr, accumulator.quant_arr, 512);
+
 	// First hidden layer
-	hidden_layer1.process_linear_layer(accumulator.quant_arr, hidden_neuros1);
+	hidden_layer1.processLinearLayer(accumulator.quant_arr, hidden_neuros1);
 	crelu(hidden_neuros1, quant_hidden_neuros1, 32);
 
 	// Second hidden layer
-	hidden_layer2.process_linear_layer(quant_hidden_neuros1, hidden_neuros2);
+	hidden_layer2.processLinearLayer(quant_hidden_neuros1, hidden_neuros2);
 	crelu(hidden_neuros2, quant_hidden_neuros2, 32);
-	
-	// Output layer
-	hidden_layer3.process_linear_layer(quant_hidden_neuros2, &output_neuron);
 
-	return output_neuron * scale;
+	// Output layer
+	hidden_layer3.processLinearLayer(quant_hidden_neuros2, &output_neuron);
+
+	return output_neuron / scale;
 }
 
 void NNUE::setPosition(const Player& player, const Player& opponent) {
-
+	accumulator.set(player, opponent);
 }
 
-void NNUE::makeMove(const unsigned short move) {
-
+void NNUE::movePiece(PieceType piece_type, location initial_loc, location final_loc, const Player& player, const Player& opponent) {
+	accumulator.movePiece(piece_type, initial_loc, final_loc, player, opponent);
 }
 
-void NNUE::unmakeMove(const unsigned short move) {
+void NNUE::addPiece(PieceType piece_type, location loc, const Player& player, const Player& opponent) {
+	accumulator.addPiece(piece_type, loc, player, opponent);
+}
 
+void NNUE::removePiece(PieceType piece_type, location loc, const Player& player, const Player& opponent) {
+	accumulator.removePiece(piece_type, loc, player, opponent);
+}
+
+void NNUE::flipSides() {
+	accumulator.flipSides();
 }
--- src/EvaluationNetwork/Evaluate/EvaluateNNUE.h
+++ src/EvaluationNetwork/Evaluate/EvaluateNNUE.h
@@ -3,28 +3,30 @@
 #include "Accumulator.h"
 #include "ClippedReLU.h"
 #include "LinearLayer.h"
+#include "PieceTypes.h"
 #include <cstdint>
 
 class NNUE {
-	Accumulator				accumulator;
-	alignas(64) int32_t		hidden_neuros1[32];
-	alignas(64) int8_t		quant_hidden_neuros1[32];
-	LinearLayer				hidden_layer1				= LinearLayer(512, 32);
-	alignas(64) int32_t		hidden_neuros2[32];
-	alignas(64) int8_t		quant_hidden_neuros2[32];
-	LinearLayer				hidden_layer2				= LinearLayer(32, 32);
-	int32_t					output_neuron;
-	LinearLayer				hidden_layer3				= LinearLayer(32, 1);
-	
+	Accumulator				accumulator = Accumulator();
+	alignas(64) int32_t		hidden_neuros1[32] = {};
+	alignas(64) int8_t		quant_hidden_neuros1[32] = {};
+	LinearLayer				hidden_layer1 = LinearLayer(512, 32);
+	alignas(64) int32_t		hidden_neuros2[32] = {};
+	alignas(64) int8_t		quant_hidden_neuros2[32] = {};
+	LinearLayer				hidden_layer2 = LinearLayer(32, 32);
+	int32_t					output_neuron = 0;
+	LinearLayer				hidden_layer3 = LinearLayer(32, 1);
+
 public:
-	// TODO: Initialize weight and biases in layers
 	NNUE();
 
 	int evaluate();
 
 	void setPosition(const Player& player, const Player& opponent);
-	void makeMove(const unsigned short move);
-	void unmakeMove(const unsigned short move);
+	void movePiece(PieceType piece_type, location initial_loc, location final_loc, const Player& player, const Player& opponent);
+	void addPiece(PieceType piece_type, location loc, const Player& player, const Player& opponent);
+	void removePiece(PieceType piece_type, location loc, const Player& player, const Player& opponent);
+	void flipSides();
 
 };
 
